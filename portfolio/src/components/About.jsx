7da494b953344ece35bea4a7f09fb721efc48e31
Nabanji <pnabanji@gmail.com>
2025-01-29 16:31:25 +0300

    Added easyhire project and contact details

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -10,17 +10,22 @@ function About() {
 
                 <div className="about-description d-flex flex-column justify-content-center">
                     <p className="h1">Bio</p>
-                    <p className='fs-5'>Prince Nabanji is a proficient software engineer with a strong foundation in front-end development technologies. With expertise in HTML, CSS, Bootstrap, JavaScript, and React, Prince excels at building responsive, dynamic, and user-friendly web applications.</p>
-                    <p className='fs-5'>His ability to seamlessly integrate modern design principles with efficient code ensures that every project he works on is optimized for both performance and user experience. Prince is passionate about creating web solutions that are not only aesthetically pleasing but also functional, scalable, and accessible.</p>
-                    <h1 className="h1">Skills</h1>
+                    <p className='fs-5'>I am a proficient software developer with a strong foundation in web development technologies. With expertise in HTML, CSS, Bootstrap, JavaScript, React, nodeJS and expressJS and MYSQL. I excel at building responsive, dynamic, and user-friendly web applications.</p>
+                    <p className='fs-5'>My ability to seamlessly integrate modern design principles with efficient code ensures that every project he works on is optimized for both performance and user experience. I am passionate about creating web solutions that are not only aesthetically pleasing but also functional, scalable, and accessible.</p>
+                    <h1 className="h3">Skills: </h1>
                     <div className="skill-icons d-flex gap-3">
                         <i className="fa-brands fa-html5 html-icon"></i>
                         <i className="fa-brands fa-css3-alt css-icon"></i>
                         <i className="fa-brands fa-bootstrap bootstrap-icon"></i>
                         <i className="fa-brands fa-js javascript-icon"></i>
                         <i className="fa-brands fa-react react-icon"></i>
-                        <i className="fa-brands fa-node-js"></i>
-                        <i className="fa-solid fa-database"></i>
+                        <i className="fa-brands fa-node-js nodejs-icon"></i>
+                        <i className="fa-solid fa-database database-icon"></i>
+                    </div>
+                    <h1 className="h3 mt-3">Technologies: </h1>
+                    <div className="technology-icons d-flex gap-2">
+                        <i className="fa-brands fa-git-alt git-icon"></i>
+                        <i className="fa-brands fa-github github-icon"></i>
                     </div>
                 </div>
 
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import eliteKicks from '../assets/Elite Kicks.png';
+import easyHire from '../assets/easyhire-sc.png';
 
 export default function Projects() {
     return (
@@ -13,7 +14,6 @@ export default function Projects() {
                     <div className="project-desc">
                         <div className="description">
                             <h2 className="h4">Elite Kicks</h2>
-                            <p className="fs-5">E-commerce design for selling shoes</p>
                         </div>
                         <div className="view">
                             <a href="https://nabanji.github.io/Elite-Kicks/" target="_blank"><i className="fa-solid fa-up-right-from-square"></i></a>
@@ -22,15 +22,13 @@ export default function Projects() {
                 </div>
 
                 <div className="project">
-                    <img src={eliteKicks} alt="project" width='100%' height='auto' />
+                    <img src={easyHire} alt="project" width='100%' height='auto' />
                     <div className="project-desc">
                         <div className="description">
-                            <h2 className="h4">Elite Kicks</h2>
-                            <p className="fs-5">E-commerce design for selling shoes</p>
-                            {/* <p className="fs-6">A modern, user-friendly e-commerce frontend featuring intuitive navigation, responsive design, and visually appealing product displays.</p> */}
+                            <h2 className="h4">EasyHire</h2>
                         </div>
                         <div className="view">
-                            <a href="https://nabanji.github.io/Elite-Kicks/" target="_blank"><i className="fa-solid fa-up-right-from-square"></i></a>
+                            <a href="https://github.com/Nabanji/EasyHire" target="_blank"><i className="fa-solid fa-up-right-from-square"></i></a>
                         </div>
                     </div>
 
